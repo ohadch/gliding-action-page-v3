e93123561d941dac94309088b011448f6e0dabbb
ohadchaet <ohad.chaet@lynx.md>
2023-09-16 08:43:30 +0300

    feat: action schema

---
+++ server/src/schemas/action.py
@@ -0,0 +1,32 @@
+from datetime import datetime
+from typing import Optional
+
+from pydantic import BaseModel
+
+
+class ActionSchema(BaseModel):
+    id: int
+    date: datetime
+    closed_at: datetime
+    field_responsible_id: int
+    responsible_cfi_id: int
+    instruction_glider_id: int
+
+    class Config:
+        orm_mode = True
+
+
+class ActionCreateSchema(BaseModel):
+    date: datetime
+    closed_at: Optional[datetime]
+    field_responsible_id: Optional[int]
+    responsible_cfi_id: Optional[int]
+    instruction_glider_id: Optional[int]
+
+
+class ActionUpdateSchema(BaseModel):
+    date: Optional[datetime]
+    closed_at: Optional[datetime]
+    field_responsible_id: Optional[int]
+    responsible_cfi_id: Optional[int]
+    instruction_glider_id: Optional[int]
