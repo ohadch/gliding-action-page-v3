f77cb7ed223f950b1a1368719c9ccdd393bd980c
ohadchaet <ohad.chaet@lynx.md>
2023-09-16 08:06:51 +0300

    feat: adds active tow airplane

---
+++ server/src/models/active_tow_airplane.py
@@ -0,0 +1,18 @@
+from sqlalchemy import Column, Integer, DateTime
+from sqlalchemy.orm import relationship
+
+from src.config.database import Base
+
+
+class ActiveTowAirplane(Base):
+
+    __tablename__ = "active_tow_airplanes"
+
+    id = Column(Integer, primary_key=True, index=True)
+    action_id = Column(Integer, nullable=False)
+    tow_pilot_id = Column(Integer, nullable=False)
+    airplane_id = Column(Integer, nullable=False)
+
+    action = relationship("Action", back_populates="active_tow_airplanes")
+    tow_pilot = relationship("User", foreign_keys=[tow_pilot_id])
+    airplane = relationship("Airplane", foreign_keys=[airplane_id])
