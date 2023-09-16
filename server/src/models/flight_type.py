3575cce79279157073b1124d71916a91beb6b395
ohadchaet <ohad.chaet@lynx.md>
2023-09-16 08:14:55 +0300

    feat: flight type

---
+++ server/src/models/flight_type.py
@@ -0,0 +1,11 @@
+from sqlalchemy import Column, Integer, String
+
+from src.config.database import Base
+
+
+class FlightType(Base):
+
+    __tablename__ = "flight_types"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, nullable=False)
