24de878c7890fdac79390896930072b8adfa9d82
ohadchaet <ohad.chaet@lynx.md>
2023-09-16 08:25:38 +0300

    feat: member

--- server/src/models/member.py
+++ server/src/models/member.py
@@ -2,12 +2,12 @@ from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import relationship
 
 from src.config.database import Base
-from src.utils.enums import AircraftTypeId
+from src.utils.enums import RoleId
 
 
-class Aircraft(Base):
+class Member(Base):
 
-    __tablename__ = "aircraft"
+    __tablename__ = "members"
 
     id = Column(Integer, primary_key=True, index=True)
     first_name = Column(String, nullable=False)
@@ -16,29 +16,8 @@ class Aircraft(Base):
     phone_number = Column(String, nullable=True)
 
     roles = relationship("Role", back_populates="member")
-    owned_aircraft = relationship("Aircraft", secondary="aircraft_owners", back_populates="owners")
+    owned_gliders = relationship("GliderOwner", back_populates="member")
 
-    @property
-    def is_secondary_tow_airplane(self):
-        return AircraftTypeId(self.type) is AircraftTypeId.SecondaryTowAirplane
-
-    @property
-    def is_main_tow_airplane(self):
-        return AircraftTypeId(self.type) is AircraftTypeId.MainTowAirplane
-
-    @property
-    def is_tow_airplane(self):
-        return self.is_main_tow_airplane or self.is_secondary_tow_airplane
-
-    @property
-    def is_self_launch_only(self):
-        return AircraftTypeId(self.type) is AircraftTypeId.TouringGlider
-
-    @property
-    def is_self_launch_possible(self):
-        return self.is_self_launch_only or AircraftTypeId(self.type) is AircraftTypeId.SelfLaunch
-
-    @property
-    def is_glider(self):
-        return not self.is_tow_airplane
+    def has_role(self, role_id: RoleId) -> bool:
+        return any(role.role_id == role_id for role in self.roles)
 
