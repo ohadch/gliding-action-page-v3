b0db519eb15838f8d0fba4a0a5200212b864f1a5
ohadchaet <ohad.chaet@lynx.md>
2023-09-16 08:14:09 +0300

    feat: adds aircraft properties

--- server/src/models/aircraft.py
+++ server/src/models/aircraft.py
@@ -1,7 +1,8 @@
-from sqlalchemy import Column, Integer, DateTime, String
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import relationship
 
 from src.config.database import Base
+from src.utils.enums import AircraftTypeId
 
 
 class Aircraft(Base):
@@ -16,5 +17,26 @@ class Aircraft(Base):
     owners = relationship("User", secondary="aircraft_owners", back_populates="aircraft")
 
     @property
+    def is_secondary_tow_airplane(self):
+        return AircraftTypeId(self.type) is AircraftTypeId.SecondaryTowAirplane
+
+    @property
+    def is_main_tow_airplane(self):
+        return AircraftTypeId(self.type) is AircraftTypeId.MainTowAirplane
+
+    @property
+    def is_tow_airplane(self):
+        return self.is_main_tow_airplane or self.is_secondary_tow_airplane
+
+    @property
+    def is_self_launch_only(self):
+        return AircraftTypeId(self.type) is AircraftTypeId.TouringGlider
+
+    @property
+    def is_self_launch_possible(self):
+        return self.is_self_launch_only or AircraftTypeId(self.type) is AircraftTypeId.SelfLaunch
+
+    @property
     def is_glider(self):
-        return self.type == 0
+        return not self.is_tow_airplane
+
