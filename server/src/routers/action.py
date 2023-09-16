697fcb374d3be89541f6f87b8e6c7dc1e8851fa9
ohadchaet <ohad.chaet@lynx.md>
2023-09-16 17:49:16 +0300

    chore: adds init to crud

--- server/src/routers/action.py
+++ server/src/routers/action.py
@@ -1,5 +1,4 @@
 from src.app import app
-from src.schemas.action import ActionCreateSchema, ActionUpdateSchema, ActionSearchSchema
 
 from src.crud.action import ActionCrud
 
@@ -10,7 +9,4 @@ crud.add_crud_routes_to_app(
     app=app,
     tags=["actions"],
     prefix="actions",
-    search_schema=ActionSearchSchema,
-    create_schema=ActionCreateSchema,
-    update_schema=ActionUpdateSchema,
 )
