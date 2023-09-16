edbaf2f824f39e893b13e43550b6e96d3dbb604b
ohadchaet <ohad.chaet@lynx.md>
2023-09-16 08:15:26 +0300

    feat: tow type

---
+++ server/src/models/tow_type.py
@@ -0,0 +1,11 @@
+from sqlalchemy import Column, Integer, String
+
+from src.config.database import Base
+
+
+class TowType(Base):
+
+    __tablename__ = "tow_types"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, nullable=False)
