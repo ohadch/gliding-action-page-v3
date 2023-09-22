ce84097eafd3aba2838ada0e880c8e4abdd21078
ohadchaet <ohad.chaet@lynx.md>
2023-09-22 10:54:14 +0300

    fix: removes input label

--- frontend/src/components/actions/ActionConfigurationComponent.tsx
+++ frontend/src/components/actions/ActionConfigurationComponent.tsx
@@ -4,13 +4,12 @@ import {
     CardContent,
     FormControl,
     FormGroup,
-    InputLabel,
     TextField
 } from "@mui/material";
 import {useTranslation} from "react-i18next";
 import {useSelector} from "react-redux";
 import {RootState, useAppDispatch} from "../../store";
-import {useEffect, useState} from "react";
+import {useEffect} from "react";
 import {fetchMembers} from "../../store/actions/member.ts";
 import {MemberSchema} from "../../lib/types.ts";
 import {setFieldResponsibleId} from "../../store/reducers/actionSlice.ts";
@@ -44,16 +43,20 @@ export default function ActionConfigurationComponent() {
             >
                 <FormGroup>
                     <FormControl>
-                        <InputLabel id="field-responsible-label">
-                            {t("FIELD_RESPONSIBLE")}
-                        </InputLabel>
                         <Autocomplete
                             id="field-responsible"
                             options={members}
                             value={fieldResponsibleId ? getMemberById(fieldResponsibleId) : null}
                             onChange={(_, newValue) => newValue && dispatch(setFieldResponsibleId(newValue.id))}
                             getOptionLabel={(option: MemberSchema) => `${option.first_name} ${option.last_name}`}
-                            renderInput={(params) => <TextField {...params} label={t("FIELD_RESPONSIBLE")} />}
+                            renderInput={(params) => {
+                                return (
+                                    <TextField
+                                        {...params}
+                                        label={t("FIELD_RESPONSIBLE")}
+                                    />
+                                )
+                            }}
                         />
                     </FormControl>
                 </FormGroup>
