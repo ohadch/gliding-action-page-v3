930402dca11e7380b73b2f21bb93412a5929203c
ohadchaet <ohad.chaet@lynx.md>
2023-09-25 08:21:26 +0300

    feat: block dispatch if flight has busy entities

--- frontend/src/pages/dashboard/DashboardPage.tsx
+++ frontend/src/pages/dashboard/DashboardPage.tsx
@@ -10,7 +10,7 @@ import {
     SelectChangeEvent
 } from "@mui/material";
 import FlightsTable from "../../components/flights/FlightsTable.tsx";
-import {useEffect, useState} from "react";
+import {useCallback, useEffect, useState} from "react";
 import Box from "@mui/material/Box";
 import {useTranslation} from "react-i18next";
 import {useSelector} from "react-redux";
@@ -24,22 +24,27 @@ import FlightStartTowDialog from "../../components/flights/FlightStartTowDialog.
 import moment from "moment/moment";
 import FlightEndTowDialog from "../../components/flights/FlightEndTowDialog.tsx";
 import {ORDERED_FLIGHT_STATES} from "../../utils/consts.ts";
+import {isFlightActive} from "../../utils/flights.ts";
+import {getMemberDisplayValue, getTowAirplaneDisplayValue} from "../../utils/display.ts";
 
 const ITEM_HEIGHT = 48;
 const ITEM_PADDING_TOP = 8;
 const MenuProps = {
-  PaperProps: {
-    style: {
-      maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
-      width: 250,
+    PaperProps: {
+        style: {
+            maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+            width: 250,
+        },
     },
-  },
 };
 
 export default function DashboardPage() {
     const [flightCreationWizardDialogOpen, setFlightCreationWizardDialogOpen] = useState<boolean>(false);
     const {t} = useTranslation();
     const {flights, fetchingFlightsInProgress, action} = useSelector((state: RootState) => state.currentAction);
+    const { members } = useSelector((state: RootState) => state.members);
+    const { gliders } = useSelector((state: RootState) => state.gliders);
+    const { towAirplanes } = useSelector((state: RootState) => state.towAirplanes);
     const dispatch = useAppDispatch();
     const [editedFlightId, setEditedFlightId] = useState<number | null>(null);
     const [editedFlightData, setEditedFlightData] = useState<FlightCreateSchema | null>(null);
@@ -47,6 +52,81 @@ export default function DashboardPage() {
     const [startTowDialogFlight, setStartTowDialogFlight] = useState<FlightSchema | null>(null);
     const [endTowDialogFlight, setEndTowDialogFlight] = useState<FlightSchema | null>(null);
     const [shownFlightStates, setShownFlightStates] = useState<FlightState[]>(["Draft", "Tow", "Inflight"]);
+
+    const activeFlights = useSelector((state: RootState) => state.currentAction.flights?.filter((flight) => isFlightActive(flight)));
+    const occupiedGliders = useCallback(() => activeFlights?.map((flight) => flight.glider_id).filter(Boolean) || [] as number[], [activeFlights])
+    const occupiedTowAirplanes = useCallback(() => activeFlights?.map((flight) => flight.tow_airplane_id).filter(Boolean) || [] as number [], [activeFlights]);
+    const occupiedMembers = useCallback(
+        () => activeFlights?.map((flight) => [flight.pilot_1_id, flight.pilot_2_id, flight.tow_pilot_id])
+            .flat()
+            .filter(Boolean) || [] as number[],
+        [activeFlights]
+    );
+
+    /**
+     * Returns a tuple of [hasOccupiedEntities, occupiedEntities]
+     */
+    const getOccupiedEntitiesFromFlight = useCallback((flight: FlightSchema) => {
+        if (!members) {
+            throw new Error("Members not loaded")
+        }
+
+        if (!gliders) {
+            throw new Error("Gliders not loaded")
+        }
+
+        if (!towAirplanes) {
+            throw new Error("Tow airplanes not loaded")
+        }
+
+        const occupiedEntities = {
+            members: [] as string[],
+            gliders: [] as string[],
+            towAirplanes: [] as string[],
+        }
+
+        if (flight.glider_id && occupiedGliders().includes(flight.glider_id)) {
+            const member = members.find((member) => member.id === flight.glider_id)
+            if (!member) {
+                throw new Error(`Member with id ${flight.glider_id} not found`)
+            }
+            occupiedEntities.gliders.push(getMemberDisplayValue(member))
+        }
+
+        if (flight.tow_airplane_id && occupiedTowAirplanes().includes(flight.tow_airplane_id)) {
+            const towAirplane = towAirplanes.find((towAirplane) => towAirplane.id === flight.tow_airplane_id)
+            if (!towAirplane) {
+                throw new Error(`Tow airplane with id ${flight.tow_airplane_id} not found`)
+            }
+            occupiedEntities.towAirplanes.push(getTowAirplaneDisplayValue(towAirplane))
+        }
+
+        if (flight.pilot_1_id && occupiedMembers().includes(flight.pilot_1_id)) {
+            const member = members.find((member) => member.id === flight.pilot_1_id)
+            if (!member) {
+                throw new Error(`Member with id ${flight.pilot_1_id} not found`)
+            }
+            occupiedEntities.members.push(getMemberDisplayValue(member))
+        }
+
+        if (flight.pilot_2_id && occupiedMembers().includes(flight.pilot_2_id)) {
+            const member = members.find((member) => member.id === flight.pilot_2_id)
+            if (!member) {
+                throw new Error(`Member with id ${flight.pilot_2_id} not found`)
+            }
+            occupiedEntities.members.push(getMemberDisplayValue(member))
+        }
+
+        if (flight.tow_pilot_id && occupiedMembers().includes(flight.tow_pilot_id)) {
+            const member = members.find((member) => member.id === flight.tow_pilot_id)
+            if (!member) {
+                throw new Error(`Member with id ${flight.tow_pilot_id} not found`)
+            }
+            occupiedEntities.members.push(getMemberDisplayValue(member))
+        }
+
+        return occupiedEntities
+    }, [occupiedGliders, occupiedTowAirplanes, occupiedMembers, members, gliders, towAirplanes])
 
     useEffect(() => {
         if (!flights && !fetchingFlightsInProgress && action) {
@@ -83,6 +163,17 @@ export default function DashboardPage() {
                 updatePayload.tow_pilot_id = null;
                 break;
             case "Tow":
+                if (getOccupiedEntitiesFromFlight(flight)) {
+                    const occupiedEntities = getOccupiedEntitiesFromFlight(flight)
+                    const hasOccupiedEntities = Object.values(occupiedEntities).some((entities) => entities.length > 0)
+
+                    if (hasOccupiedEntities) {
+                        const occupiedEntitiesString = Object.values(occupiedEntities).flat().join(", ")
+
+                        return alert(`${t("FLIGHT_HAS_OCCUPIED_ENTITIES_ALERT")}: ${occupiedEntitiesString}`)
+                    }
+                }
+
                 if (!flight.tow_airplane_id || !flight.tow_pilot_id) {
                     return setStartTowDialogFlight(flight);
                 }
