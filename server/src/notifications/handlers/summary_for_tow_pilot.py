83921fee368fa45e00624b21320b5b9beaa8e4d4
ohadchaet <ohad.chaet@lynx.md>
2023-09-29 15:28:44 +0300

    feat: notification for tow pilot upon unassignment

---
+++ server/src/notifications/handlers/summary_for_tow_pilot.py
@@ -0,0 +1,30 @@
+from src import Notification
+from src.emails.email_client import EmailClient
+from src.i18n import i18n_client_factory
+from src.notifications.handlers import NotificationHandler
+from src.notifications.types import NotificationPayloadSchema
+
+
+class FlightSummaryForPilotNotificationHandler(NotificationHandler):
+    def _send_via_email(self, notification: Notification) -> None:
+        email_client = EmailClient()
+        i18n = i18n_client_factory()
+        flight = self._get_flight(notification=notification)
+
+        subject = i18n.get_flight_summary_for_pilot_email_message_subject(flight=flight)
+        message = i18n.get_flight_summary_for_pilot_email_message(flight=flight)
+
+        email_client.send_email(
+            to_email=notification.recipient_member.email,
+            subject=subject,
+            html_content=message,
+        )
+
+    def _send_via_console(self, notification: Notification) -> None:
+        payload = NotificationPayloadSchema(**notification.payload)
+        flight_id = payload.flight_ids[0]
+
+        subject = f"Summary for tow pilot"
+        message = f"Flight {flight_id} has landed."
+
+        print(f"{subject}/{message}")
--- server/src/notifications/types.py
+++ server/src/notifications/types.py
@@ -5,3 +5,5 @@ from pydantic import BaseModel
 
 class NotificationPayloadSchema(BaseModel):
     flight_ids: List[int]
+    tow_pilot_id: int
+    tow_airplane_id: int
