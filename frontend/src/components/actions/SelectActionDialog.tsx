d38fe37be40e6d6fa89cef45f9275562a68f8442
ohadchaet <ohad.chaet@lynx.md>
2023-10-05 15:56:52 +0300

    bugfix

--- frontend/src/components/actions/SelectActionDialog.tsx
+++ frontend/src/components/actions/SelectActionDialog.tsx
@@ -6,7 +6,7 @@ import {
     Button, Grid, Tooltip,
 
 } from "@mui/material";
-import React, {useEffect} from "react";
+import {useEffect} from "react";
 import {useTranslation} from "react-i18next";
 import {useSelector} from "react-redux";
 import {RootState, useAppDispatch} from "../../store";
