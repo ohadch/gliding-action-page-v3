aba17b961b23b5b69511973d18cc9666ef3c5e99
ohadchaet <ohad.chaet@lynx.md>
2023-10-05 21:06:27 +0300

    feat: headers for notifications and events pages

--- frontend/src/pages/events/EventsPage.tsx
+++ frontend/src/pages/events/EventsPage.tsx
@@ -1,9 +1,14 @@
 import EventsTable from "../../components/events/EventsTable.tsx";
 import {Grid} from "@mui/material";
+import {useTranslation} from "react-i18next";
 
 export default function EventsPage() {
+    const {t} = useTranslation()
     return (
         <Grid>
+            <Grid>
+                <h1>{t("EVENTS")}</h1>
+            </Grid>
             <EventsTable/>
         </Grid>
     )
--- frontend/src/pages/notifications/NotificationsPage.tsx
+++ frontend/src/pages/notifications/NotificationsPage.tsx
@@ -1,9 +1,14 @@
 import {Grid} from "@mui/material";
 import NotificationsTable from "../../components/notifications/NotificationsTable.tsx";
+import {useTranslation} from "react-i18next";
 
 export default function NotificationsPage() {
+    const {t} = useTranslation()
     return (
         <Grid>
+            <Grid>
+                <h1>{t("NOTIFICATIONS")}</h1>
+            </Grid>
             <NotificationsTable/>
         </Grid>
     )
